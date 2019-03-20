d534b9df646f01a8deaefc2c19293b200ea6241e
Lauren Fowler <lauren.michelle.fowler@live.mercer.edu>
2019-03-20 17:39:15 -0400

    working as expected so far

--- labs/lab4/mp3.cpp
+++ labs/lab4/mp3.cpp
@@ -40,6 +40,7 @@ class Artist {
      int time;    // Total time of all songs on all albums by this artist
      int nsongs;  // Total number of songs on all albums by this artist
      bool operator < (Artist another) const { return name < another.name;}
+     bool operator == (Artist another) const {return name == another.name;}
 };
 
 typedef multimap <Artist, Album> mp3map;
@@ -54,11 +55,18 @@ int main(int argc, char* argv[]){
     mp3map records; 
     mp3map:: iterator it;   
 
+    if(argc < 2){
+        cerr << "mp3: missing filename 'mp3'" << endl;
+        exit(0);
+    }
 
     //open the file
     fstream infile(argv[1], ios::in);
     
     while(getline(infile, instr)){
+        map <int, Song> songs;
+        bool exists = false;
+        bool same_album = false;
         stringstream tmp(instr); //so it doesnt think its reached eof
         //get all info into vars
         tmp >> title >> time >> artist >> album >> genre >> track;
@@ -68,14 +76,80 @@ int main(int argc, char* argv[]){
         time.erase(loc, 1);
         int new_time = stoi(time);
 
-        Artist tmp_art;
-        tmp_art.name = clean_up(artist, '_');
-        tmp_art.time += new_time;
-        cout << new_time << endl;;
+        //create song entry
+        Song new_song;
+        new_song.title = clean_up(title, '_');
+        new_song.time = new_time;
+        new_song.track = track;  
+
+        Artist new_art;
+        new_art.name = clean_up(artist, '_');
+        Artist store;
+
+        for(it = records.begin(); it != records.end(); it++){
+            Artist cmp = it -> first;
+            Album cmpA = it -> second;
+            if(cmp == new_art){
+                exists = true; //turn on flag so if wont run
+                //update artist information for each instance
+                cmp.time+=new_time;
+                cmp.nsongs++;
+
+                //check for album match
+                if(cmpA.name == clean_up(album, '_')){
+                    cout << "new song" << endl;
+                    same_album = true; //turn on flag so if wont run
+                    //update album info
+                    cmpA.time+=new_time;
+                    cmpA.nsongs++;
+                    songs = cmpA.songs;
+                    songs[track] = new_song;
+                }
+                else{
+                    new_art = cmp; //store for new album entry
+                }  
+            }
+        }
+
+        //creates a new entry if it is a new album and artist already exists
+        if(!same_album && exists){
+            cout << "same artist, new album" << endl;
+            //create new album, same artist
+            Album new_album;
+            new_album.name = clean_up(album, '_');
+            new_album.genre = clean_up(album, '_');
+            new_album.time+=new_time;
+            songs = new_album.songs;
+            songs[track] = new_song;
+            records.insert({new_art, new_album});
+
+        }
+
+        //create a new entry
+        if(!exists){
+            cout << "new artist" << endl;
+            //update artist info
+            new_art.time+=new_time;
+            new_art.nsongs++;
+
+            //create album
+            Album new_album;
+            new_album.name = clean_up(album, '_');
+            new_album.genre = clean_up(album, '_');
+            new_album.time+=new_time;
+            songs = new_album.songs;
+            songs[track] = new_song;
+            records.insert({new_art, new_album});
+        }    
             
-       }           
+    }          
   
-    cout << "exit program" << endl;
+    Artist artist_obj;
+
+    for(it = records.begin(); it!= records.end(); it++){
+        artist_obj = it -> first;
+        cout << artist_obj.name << endl;
+    } 
 
     return 0;
 }
